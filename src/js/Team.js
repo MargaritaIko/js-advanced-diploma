67751d54737de39f530c49899dfba15017552943
Margarita May Lee <ritochka.m_lee@yahoo.com>
2023-03-26 18:15:16 +0300

    First commit

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,16 +1,20 @@
-/**
- * Класс, представляющий персонажей команды
- *
- * @todo Самостоятельно продумайте хранение персонажей в классе
- * Например
- * @example
- * ```js
- * const characters = [new Swordsman(2), new Bowman(1)]
- * const team = new Team(characters);
- *
- * team.characters // [swordsman, bowman]
- * ```
- * */
+import Bowman from './Characters/bowman';
+import Daemon from './Characters/daemon';
+import Magician from './Characters/magician';
+import Swordsman from './Characters/swordsman';
+import Undead from './Characters/undead';
+import Vampire from './Characters/vampire';
+
 export default class Team {
-  // TODO: write your logic here
+  static getStartPlayerTeam() {
+    return [new Bowman(1), new Swordsman(1)];
+  }
+
+  static getPlayerTeam() {
+    return [Bowman, Swordsman, Magician];
+  }
+
+  static getComputerTeam() {
+    return [Daemon, Undead, Vampire];
+  }
 }
